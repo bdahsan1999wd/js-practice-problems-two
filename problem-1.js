ea9bd65b846cb03a420796baf0b1b8ad172739da
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-01-28 08:11:47 +0600

    js-practice-problems-two

---
+++ problem-1.js
@@ -0,0 +1,15 @@
+var fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+
+var bananaIndex = fruits.indexOf('banana');
+console.log(bananaIndex);
+
+// fruits[1] = 'mango'
+fruits[bananaIndex] = 'mango'
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('watermelon');
+console.log(fruits);
---
+++ problem-2.js
@@ -0,0 +1,85 @@
+var myScore = 85;
+var tomScore = 66;
+var janeScore = 95;
+var peterScore = 56;
+var johnScore = 40;
+// -------------------------My Score-------------------------
+if (myScore >= 80) {
+    console.log('My grade is A');
+}
+else if (myScore >= 60) {
+    console.log('My grade is B');
+}
+else if (myScore >= 50) {
+    console.log('MY grade is C');
+}
+else if (myScore >= 40) {
+    console.log('My grade is D');
+}
+else {
+    console.log('My grade is F')
+}
+// -------------------------Tom Score-------------------------
+if (tomScore >= 80) {
+    console.log('tom grade is A');
+}
+else if (tomScore >= 60) {
+    console.log('tom grade is B');
+}
+else if (tomScore >= 50) {
+    console.log('tom grade is C');
+}
+else if (tomScore >= 40) {
+    console.log('tom grade is D');
+}
+else {
+    console.log('tom grade is F')
+}
+// ---------------------------Jane Score-------------------------
+if (janeScore >= 80) {
+    console.log('jane grade is A');
+}
+else if (janeScore >= 60) {
+    console.log('jane grade is B');
+}
+else if (janeScore >= 50) {
+    console.log('jane grade is C');
+}
+else if (janeScore >= 40) {
+    console.log('jane grade is D');
+}
+else {
+    console.log('jane grade is F')
+}
+// --------------------------Peter Score-------------------------
+if (peterScore >= 80) {
+    console.log('peter grade is A');
+}
+else if (peterScore >= 60) {
+    console.log('peter grade is B');
+}
+else if (peterScore >= 50) {
+    console.log('peter grade is C');
+}
+else if (peterScore >= 40) {
+    console.log('peter grade is D');
+}
+else {
+    console.log('peter grade is F')
+}
+// -------------------------John Score-------------------------
+if (johnScore >= 80) {
+    console.log('john grade is A');
+}
+else if (johnScore >= 60) {
+    console.log('john grade is B');
+}
+else if (johnScore >= 50) {
+    console.log('john grade is C');
+}
+else if (johnScore >= 40) {
+    console.log('john grade is D');
+}
+else {
+    console.log('john grade is F')
+}
