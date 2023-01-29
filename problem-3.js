fbe43fd26fe8f5f06754d51c094ecfa29d28fcd8
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-01-30 00:46:03 +0600

    some problem solved

--- problem-3.js
+++ problem-3.js
@@ -2,12 +2,37 @@ var num1 = 13;
 var num2 = 79;
 var num3 = 45;
 
-if (num1 >= num2 && num1 >= num3) {
+if (num1 > num2 && num1 > num3) {
     console.log('Number-', num1);
 }
-else if (num2 >= num1 && num2 >= num3) {
+else if (num2 > num1 && num2 > num3) {
     console.log('Number-', num2);
 }
 else {
     console.log('Number-', num3);
 }
+
+/* ----------------------------------other way------------------------------
+
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+if(num1 > num2){
+    if(num1 > num3){
+        console.log(num1);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else{
+    if(num2 > num3){
+        console.log(num2);
+    }
+    else{
+        console.log(num3);
+    }
+}
+
+*/
