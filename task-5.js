d31add0dd77df361716563de663f44dab0e5f786
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-07-31 23:50:01 +0600

    task-5 added

---
+++ task-5.js
@@ -0,0 +1,23 @@
+// --------------------Simple if-else--------------------
+let num1 = 10;
+let num2 = 5;
+
+let result;
+
+if (num1 > num2) {
+    result = num1 * 2;
+}
+else {
+    result = num1 + num2;
+}
+
+console.log(result);
+
+
+// --------------------Simple ternary operator--------------------
+let number1 = 110;
+let number2 = 50;
+
+let results = (number1 > number2) ? number1 * 2 : number1 + number2;
+
+console.log(results);
